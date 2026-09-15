ad9e7f7877e1e8a7e7ca52063a5f13e94b80d58d
lkerison <l.kerison@escolar.ifrn.edu.br>
2026-09-15 10:03:28 -0300

    Format code and adjust spacing in sprites.py

--- sprites.py
+++ sprites.py
@@ -1,13 +1,13 @@
-import pygame
+import pygame 
 
 
-class Entidade(pygame.sprite.Sprite):
-    def __init__(self, x, y, velocidade):
-        super().__init__()
-        self.velocidade = velocidade
-        self.image = pygame.Surface((40, 40))
-        self.rect = self.image.get_rect(center=(x, y))
+class Entidade (pygame .sprite .Sprite ):
+    def __init__ (self ,x ,y ,velocidade ):
+        super ().__init__ ()
+        self .velocidade =velocidade 
+        self .image =pygame .Surface ((40 ,40 ))
+        self .rect =self .image .get_rect (center =(x ,y ))
 
-    def mover(self, dx, dy):
-        self.rect.x += dx
-        self.rect.y += dy
+    def mover (self ,dx ,dy ):
+        self .rect .x +=dx 
+        self .rect .y +=dy 
